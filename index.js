8e6af6f92a1e9b76893ad21ee2c325fa9fddeac8
Suzi Nogueira <asuzinogueira@gmail.com>
2021-09-19 20:07:58 -0300

    usando for, if e partials

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-let user = {
+let users = [
+    {
+    id: 0,
     name: "Suzi",
     phone: "(11)123412345"
-}
+    },
+    {
+    id: 1,
+    name: "José",
+    phone: "(11)123412345"
+    },
+    {
+    id: 2,
+    name: "Maria",
+    phone: "(11)123412345"
+    }
+            
+
+]
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
@@ -14,8 +29,12 @@ app.set("view engine", "ejs");
 
 app.get("/", (req,res) =>{
 
-    res.render('user', user);
+    res.render('user', {users:users});
 
+})
+
+app.get("/about", (req, res) =>{
+    res.render('about');
 })
 
 app.listen(3000, ()=>{
